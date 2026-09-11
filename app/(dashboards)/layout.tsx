f529ecda7ffd122bfea7cba07e54161ca91e8d98
Alao Miracle <miraclealao24@gmail.com>
2026-09-11 02:04:23 +0100

    Hydration error fixed

--- app/(dashboards)/layout.tsx
+++ app/(dashboards)/layout.tsx
@@ -2,15 +2,12 @@ import DashboardLayout from "../components/DashboardComponents/DashboardLayout";
 import "../globals.css";
 import { AuthProvider } from "@/context/AuthContext";
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function DashboardsLayout({ children }: { children: React.ReactNode }) {
   return (
-    <body className="min-h-screen bg-[#f9fafb] text-gray-900">
-        <AuthProvider>
-          <DashboardLayout>
-            {children}
-          </DashboardLayout>
-        </AuthProvider>
-      </body>
- 
+    <AuthProvider>
+      <DashboardLayout>
+        {children}
+      </DashboardLayout>
+    </AuthProvider>
   );
 }
